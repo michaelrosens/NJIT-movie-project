5efd187d4a2c5fc8c4877f600db8eca046d1f9a6
MICHAEL_ROSENSTRAUS <rrosenstrausmichael@gmail.com>
2024-12-12 11:28:38 -0500

    Step 5-7

--- site.js
+++ site.js
@@ -22,25 +22,28 @@
 
 
 const vue_app = Vue.createApp({
-    // This automatically imports your movies.json file and puts it into
-    //   the variable: movies
-    created () {
-          fetch('movies.json').then(response => response.json()).then(json => {
-                this.movies = json
-          })
-    },
-    data() {
-      return {
-          // This holds your movies.json data.
+      created () {
+        fetch('movies.json').then(response => response.json()).then(json => {
+          this.movies = json
+        })
+      },
+      data() {
+        return {
           movies: [],
           title: "IMDB + Vue.js",
           owner: "Michael Rosenstraus",
           github: "https://github.com/michaelrosens"       
-    }
-  },
-    methods: {
-          /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-    }
-})
-
-vue_app.mount("#vue_app")
+        }
+      },
+      methods: {
+        posterClick(movie) {
+          movie.posterindex++;
+          if (movie.posterindex >= movie.posters.length) {
+            movie.posterindex = 0;
+          }
+        }
+      }
+    })
+    
+    vue_app.mount("#vue_app")
+    
